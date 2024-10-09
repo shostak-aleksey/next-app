e7ad6b76299664cd1685c2bbbdda86317743eccf
Alex <shostak-aleshka@mail.ru>
2024-10-09 04:36:39 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
-          width={180}
+          width={200}
           height={38}
           priority
         />
